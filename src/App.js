7fc0fc4ec35e5a5312eaa275f79f3a1a08887986
Christian Vasquez Leon <vasquezleon.christian@gmail.com>
2023-11-17 14:43:35 -0600

    npm run format

--- src/App.js
+++ src/App.js
@@ -2,63 +2,97 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [display, setDisplay] = useState('0');
+  const [display, setDisplay] = useState("0");
 
   const handleNumber = (event) => {
     const num = event.target.textContent;
 
-    if (display === '0') {
+    if (display === "0") {
       setDisplay(num);
     } else {
-      setDisplay(display + num)
+      setDisplay(display + num);
     }
-  }
+  };
 
   const handleOperator = (event) => {
     const operator = event.target.textContent;
-    setDisplay(display + ' ' + operator + ' ')
-  }
+    setDisplay(display + " " + operator + " ");
+  };
 
   const handleEqual = (event) => {
-    setDisplay(eval(display))
-  }
+    setDisplay(eval(display));
+  };
 
   const handleDecimal = () => {
-    const arrayNumbers = display.split(' ')
-    const lastElement = arrayNumbers[arrayNumbers.length - 1]
+    const arrayNumbers = display.split(" ");
+    const lastElement = arrayNumbers[arrayNumbers.length - 1];
 
-    if (!lastElement.includes('.') && !isNaN(parseInt(lastElement)) ) {
-      setDisplay(display + '.')
+    if (!lastElement.includes(".") && !isNaN(parseInt(lastElement))) {
+      setDisplay(display + ".");
     }
-  }
+  };
 
   const handleClear = () => {
-    setDisplay('0')
-  }
+    setDisplay("0");
+  };
 
   return (
     <div className="App">
       <div className="calculator">
-        <div id="display" className="row">{display}</div>
+        <div id="display" className="row">
+          {display}
+        </div>
         <div id="clear" className="row" onClick={handleClear}>
           AC
         </div>
-        <div id="seven" onClick={handleNumber}>7</div>
-        <div id="eight" onClick={handleNumber}>8</div>
-        <div id="nine" onClick={handleNumber}>9</div>
-        <div id="multiply" onClick={handleOperator}>*</div>
-        <div id="four" onClick={handleNumber}>4</div>
-        <div id="five" onClick={handleNumber}>5</div>
-        <div id="six" onClick={handleNumber}>6</div>
-        <div id="divide" onClick={handleOperator}>/</div>
-        <div id="one" onClick={handleNumber}>1</div>
-        <div id="two" onClick={handleNumber}>2</div>
-        <div id="three" onClick={handleNumber}>3</div>
-        <div id="add" onClick={handleOperator}>+</div>
-        <div id="zero" onClick={handleNumber}>0</div>
-        <div id="decimal" onClick={handleDecimal}>.</div>
-        <div id="equals" onClick={handleEqual}>=</div>
-        <div id="subtract" onClick={handleOperator}>-</div>
+        <div id="seven" onClick={handleNumber}>
+          7
+        </div>
+        <div id="eight" onClick={handleNumber}>
+          8
+        </div>
+        <div id="nine" onClick={handleNumber}>
+          9
+        </div>
+        <div id="multiply" onClick={handleOperator}>
+          *
+        </div>
+        <div id="four" onClick={handleNumber}>
+          4
+        </div>
+        <div id="five" onClick={handleNumber}>
+          5
+        </div>
+        <div id="six" onClick={handleNumber}>
+          6
+        </div>
+        <div id="divide" onClick={handleOperator}>
+          /
+        </div>
+        <div id="one" onClick={handleNumber}>
+          1
+        </div>
+        <div id="two" onClick={handleNumber}>
+          2
+        </div>
+        <div id="three" onClick={handleNumber}>
+          3
+        </div>
+        <div id="add" onClick={handleOperator}>
+          +
+        </div>
+        <div id="zero" onClick={handleNumber}>
+          0
+        </div>
+        <div id="decimal" onClick={handleDecimal}>
+          .
+        </div>
+        <div id="equals" onClick={handleEqual}>
+          =
+        </div>
+        <div id="subtract" onClick={handleOperator}>
+          -
+        </div>
       </div>
       ;
     </div>
