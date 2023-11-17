a2245596cf7d08fc6c0cd1640fd178ec9e309c16
Christian Vasquez Leon <vasquezleon.christian@gmail.com>
2023-11-17 14:05:02 -0600

    Add working calculator design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [display, setDisplay] = useState('0');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div id="display" className="row">{display}</div>
+        <div id="clear" className="row">
+          AC
+        </div>
+        <div id="seven">7</div>
+        <div id="eight">8</div>
+        <div id="nine">9</div>
+        <div id="multiply">*</div>
+        <div id="four">4</div>
+        <div id="five">5</div>
+        <div id="six">6</div>
+        <div id="divide">/</div>
+        <div id="one">1</div>
+        <div id="two">2</div>
+        <div id="three">3</div>
+        <div id="add">+</div>
+        <div id="zero">0</div>
+        <div id="decimal">.</div>
+        <div id="equals">=</div>
+        <div id="subtract">-</div>
+      </div>
+      ;
     </div>
   );
 }
